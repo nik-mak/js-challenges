494eb124c70e79478a686fa08d5d25e0ad253936
nik-mak <nick.makrakis@gmail.com>
2022-05-27 09:36:00 +1000

    challenege complete

--- a/atomic-blonde/assets/answer.js
+++ a/atomic-blonde/assets/answer.js
@@ -1,3 +1,16 @@
-export const atomicBlonde = () => {
-    // Your code here
-};
+// export const atomicBlonde = (num) => {
+//     // Your code here
+//     const digits = num.toString().split('').map(Number)
+//     console.log(digits)
+// };
+
+const atomicBlonde = (num) => {
+  const digits = num.toString().split('').map(Number)
+  const sum = digits.reduce((a, b) => a * b, 0)
+  const mult = digits.reduce((a, b) => a * b, 1)
+  return sum === mult ? `${num} is an atomic blonde number` : `${num} is not an atomic blonde number`  
+}
+
+
+console.log(atomicBlonde(123))
+console.log(atomicBlonde(456))
--- b/magic-number/assets/answer.js
+++ b/magic-number/assets/answer.js
@@ -1,3 +1,30 @@
-export const magicNumber = () => {
-    // Your code here
-};
+// export const magicNumber = () => {
+//     // Your code here
+
+// };
+const magicNumber = (num) => {
+  let count = 0
+  do {
+    const ascending = num.toString().split('').map(Number).sort((a, b) => a - b)
+    const descending = num.toString().split('').map(Number).sort((a, b) => b - a)
+  
+    const smallest = ascending.join('')
+    const largest = descending.join('')
+
+    num = largest - smallest
+    count++
+  } while (num != 6174)
+  return count
+}
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Enter a 4 digit number with at least 2 different numbers: ', number => {
+  console.log(`It takes ${magicNumber(number)} iterations to get to the magic number`);
+  readline.close();
+});
+
+magicNumber(8082)
