cd5f079228665ddc6e3d6691f2dc9b9e31136db7
nik-mak <nick.makrakis@gmail.com>
2022-05-27 12:00:19 +1000

    challenege complete

--- different-loops/assets/answer.js
+++ different-loops/assets/answer.js
@@ -1,3 +1,18 @@
-export const differentLoops = () => {
-    // Your code here
-};
+// export const differentLoops = () => {
+//     // Your code here
+// };
+
+function differentLoops(array) {
+  for (let element of array) {
+    console.log(element)
+  }
+
+  function display(item) {
+    console.log(item)
+  }
+  array.forEach(display)
+
+  array.forEach(item => console.log(item))
+
+  array.reduce((a, b) => a + b, 0)
+}
--- goldfish/assets/answer.js
+++ goldfish/assets/answer.js
@@ -1,3 +1,26 @@
-export const goldfish = () => {
-    // Your code here
-};
+// export const goldfish = () => {
+//     // Your code here
+// };
+
+const prompt = require('prompt-sync')();
+
+arr = []
+function goldfish(input) {
+  arr.push(input)
+  if (arr.length > 5) {
+    arr.shift()
+  }
+  arr.forEach(entry => console.log(entry))
+}
+
+let exit = false
+
+while (!exit) {
+  let entry = prompt('Dear Diary: ')
+
+  if (entry == "exit") {
+    exit = true
+  } else {
+    goldfish(entry)
+  }
+}
